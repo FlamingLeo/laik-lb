dc796ed448586aa41928cc7a5c585928db43bc62
Flavius Schmidt <61120933+FlamingLeo@users.noreply.github.com>
2025-10-12 22:41:04 +0200

    add temporary smoothing example to show smoothing feature

---
+++ examples/lbsmooth.c
@@ -0,0 +1,124 @@
+// smoothing test
+#include <laik.h>
+#include <laik-internal.h>
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define CSVNAME "array_data_lb.csv"
+
+////////////////////////
+// iteration examples //
+////////////////////////
+
+// 2d example
+int main_2d(int argc, char *argv[], int64_t sdsize, int lcount, Laik_LBAlgorithm lbalg, bool do_visualization)
+{
+    // initialization
+    Laik_Instance *inst = laik_init(&argc, &argv);
+    Laik_Group *world = laik_world(inst);
+    int id = laik_myid(world);
+
+    laik_lbvis_enable_trace(id, inst);
+    laik_svg_profiler_enter(inst, __func__);
+
+    // laik_lb_config_smoothing(1, -1, -1, -1);
+
+    if (id == 0)
+    {
+        printf("Running 2D example with %d iterations.\n", lcount);
+        printf("Space size %ldx%ld = %ld.\n", sdsize, sdsize, sdsize * sdsize);
+        printf("Using LB algorithm: %s\n", laik_get_lb_algorithm_name(lbalg));
+        printf("Visualisation: %d\n", do_visualization);
+    }
+
+    Laik_Space *space = laik_new_space_2d(inst, sdsize, sdsize);
+    Laik_Data *data = laik_new_data(space, laik_Int32);
+    Laik_Partitioner *parter = laik_new_bisection_partitioner();
+    Laik_Partitioning *part = laik_new_partitioning(parter, world, space, 0);
+    laik_switchto_partitioning(data, part, LAIK_DF_None, LAIK_RO_None);
+
+    int iterations = (id + 1) * 10;
+    double c = (512.0 / (double)sdsize) * (512.0 / (double)sdsize) * 0.5; // sleep time constant multiplier (def. max: ~1.31s)
+
+    Laik_Timer t = {0};
+    laik_timer_start(&t);
+
+    // modified version of the 1d example, to be changed to something more "professional" later (e.g. n-body)
+    for (int loop = 0; loop < lcount; ++loop)
+    {
+        laik_log(1, "%d ranges\n", laik_my_rangecount(part));
+        laik_lb_balance(START_LB_SEGMENT, 0, 0);
+
+        // if (id == 0 && loop == 4) sleep(1);
+
+        for (int iter = 0; iter < iterations; ++iter)
+        {
+            uint64_t sleepdur = 0;
+            for (int r = 0; r < laik_my_rangecount(part); ++r)
+            {
+                int64_t from_x, from_y, to_x, to_y;
+                laik_my_range_2d(part, r, &from_x, &to_x, &from_y, &to_y);
+                sleepdur += (to_x - from_x) * (to_y - from_y);
+            }
+            usleep((uint64_t)((double)sleepdur * c));
+        }
+
+        // calculate and switch to new partitioning determined by load balancing algorithm
+        Laik_Partitioning *newpart = laik_lb_balance(STOP_LB_SEGMENT, part, lbalg);
+        laik_lb_switch_and_free(&part, &newpart, data, LAIK_DF_Preserve);
+    }
+
+    double time = laik_timer_stop(&t);
+    if (id == 0)
+        printf("Done. Time taken: %fs\n", time);
+
+    // visualize task ranges
+    if (do_visualization)
+    {
+        if (id == 0)
+        {
+            Laik_RangeList *lr = laik_partitioning_myranges(part);
+            laik_lbvis_export_partitioning(CSVNAME, lr);
+            laik_lbvis_visualize_partitioning(CSVNAME);
+        }
+    }
+
+    // done
+    laik_svg_profiler_exit(inst, __func__);
+    laik_svg_profiler_export_json(inst);
+    laik_finalize(inst);
+    if (id == 0)
+        laik_lbvis_save_trace();
+    return 0;
+}
+
+// choose example and parameters based on input
+//
+// USAGE: <mpirun -n x> ./lb [example] <do_vis> <lb_algo> <spacesize> <loopcount>
+int main(int argc, char *argv[])
+{
+    // prepare program trace visualization
+    laik_lbvis_remove_visdata();
+
+    // optional space size and loop count arguments
+    int64_t sidelen = 0;
+    char *algo = NULL;
+    bool do_visualization = false;
+    int lcount = 15; // loop count, increase this to test thresholds
+    if (argc > 2)
+        do_visualization = atoi(argv[2]);
+    if (argc > 3)
+        algo = argv[3];
+    if (argc > 4)
+        sidelen = atoi(argv[4]);
+    if (argc > 5)
+        lcount = atoi(argv[5]);
+
+    if (sidelen == 0)
+        sidelen = 1024;
+    main_2d(argc, argv, sidelen, lcount, algo ? laik_strtolb(algo) : LB_HILBERT, do_visualization);
+}
